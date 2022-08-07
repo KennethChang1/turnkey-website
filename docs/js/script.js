46e00302527003c2967aa08667b46913c81f6740
Kenneth Chang <kenneth.kristopher27@gmail.com>
2022-08-07 22:42:11 +0700

    add docs file

---
+++ docs/js/script.js
@@ -0,0 +1,20 @@
+//Get the button
+var goto = document.getElementById("goto");
+goto.addEventListener("click", topFunction);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    goto.style.display = "block";
+  } else {
+    goto.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
