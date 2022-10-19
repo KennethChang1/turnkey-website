abb507947e7774cc6067245c6af2b8b1df2d2945
Kenneth Chang <kenneth.kristopher27@gmail.com>
2022-10-19 16:59:40 +0700

    add files

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,19 @@
 //Get the button
 var goto = document.getElementById("goto");
 goto.addEventListener("click", topFunction);
+const career = document.querySelector(".nav-career");
+console.log(career);
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
     goto.style.display = "block";
   } else {
     goto.style.display = "none";
@@ -18,3 +25,10 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+window.onload = function () {
+  var el = document.getElementById("career-jobs");
+  el.scrollIntoView({
+    behavior: "smooth",
+  });
+};
